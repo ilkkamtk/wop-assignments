318361b3ed8e613cb6b9f4cfa724cdde4b3d9f8c
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-11-02 15:12:12 +0200

    middleware

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -12,7 +12,13 @@ const cat_get = (req, res) => {
   res.json(cat);
 };
 
+const cat_post = (req, res) => {
+  console.log('cat_post', req.body, req.file);
+  res.send('Cat post done.');
+};
+
 module.exports = {
   cat_list_get,
   cat_get,
+  cat_post,
 };
