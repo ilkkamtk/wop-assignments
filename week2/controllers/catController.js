6dee77339c6186b76eefd777480a6e82a19c54a8
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2022-11-03 12:03:42 +0200

    database

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -27,8 +27,14 @@ const cat_post = async (req, res) => {
   ];
 
   const result = await addCat(data);
-  console.log('addCat', result);
-  res.send('Cat post done.');
+  if(result.affectedRows > 0) {
+    res.json({
+      message: 'cat added',
+      cat_id: result.insertId,
+    });
+  } else {
+    res.send('virhe')
+  }
 };
 
 module.exports = {
